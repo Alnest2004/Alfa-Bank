d562624bcb002654d0138f19c2b31b0623b3748b
Aleksandr <aleksdoterov@gmail.com>
2022-04-03 01:13:19 +0300

    Add static files

--- alfa_bank/internet_banking/urls.py
+++ alfa_bank/internet_banking/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from rest_framework import routers
 
+from internet_banking.views import HomePageView
 
 urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
 
 ]
