6d9c54a842189e9623df6cfeef7489680403b73e
Aleksandr <aleksdoterov@gmail.com>
2022-04-13 13:08:19 +0300

    made a transfer "80%". Need to add commission

---
+++ alfa_bank/internet_banking/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+from django.core.exceptions import ValidationError
+
+from internet_banking.models import Account, Customer, Transfer
+from users.models import User
+
+
+class CreateTransferForm(forms.ModelForm):
+    # widget.attrs['readonly'] = True
+    commission = forms.DecimalField(label='Комиссия',
+                                    widget=forms.TextInput(attrs={'readonly': 'readonly'}),
+                                    initial=1.2,)
+
+    def clean_to_account(self):
+        form_account = self.cleaned_data.get('to_account')
+        existing = User.objects.filter(username=form_account)
+        if not existing:
+            raise ValidationError('Такого пользователя нету')
+
+        return form_account
+
+
+    class Meta:
+        model = Transfer
+        exclude = ['from_account']
+
